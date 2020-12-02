c49d8e35b2774c664521810ac7a2a91f677a36e3
César Jaldin <rodia.piedra@gmail.com>
2020-12-01 22:47:51 -0400

    Fix error during push process

--- src/main/java/com/workplace/simon/repository/RegisterRepository.java
+++ src/main/java/com/workplace/simon/repository/RegisterRepository.java
@@ -2,8 +2,10 @@ package com.workplace.simon.repository;
 
 import com.workplace.simon.model.Register;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface RegisterRepository extends JpaRepository<Register, UUID> {
 }
