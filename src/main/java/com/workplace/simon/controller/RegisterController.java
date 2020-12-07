97830d6b6e95d376230edf6e7df725bec5abb1c3
César Jaldin <rodia.piedra@gmail.com>
2020-12-07 11:08:00 -0400

    Update the table to access from the views

--- src/main/java/com/workplace/simon/controller/RegisterController.java
+++ src/main/java/com/workplace/simon/controller/RegisterController.java
@@ -33,6 +33,6 @@ public class RegisterController {
 
         this.getRegisterService().save(register);
 
-        return "registered-user";
+        return "index";
     }
 }
--- src/main/java/com/workplace/simon/model/Register.java
+++ src/main/java/com/workplace/simon/model/Register.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.util.UUID;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class Register {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
