52a28c44fe60aa0588ce7f074ba405246776c657
César Jaldin <rodia.piedra@gmail.com>
2020-12-25 21:03:19 -0400

    Fix to authorization in sidebar menu

--- src/main/java/com/workplace/simon/service/UserDetailsServiceImplement.java
+++ src/main/java/com/workplace/simon/service/UserDetailsServiceImplement.java
@@ -32,7 +32,7 @@ public class UserDetailsServiceImplement implements UserDetailsService {
         if (user == null) throw new UsernameNotFoundException(username);
 
         Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
-        for (Role role : user.getRoles()){
+        for (Role role : user.getRoles()) {
             grantedAuthorities.add(new SimpleGrantedAuthority(role.getName()));
         }
 
