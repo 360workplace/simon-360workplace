4f5ed71dc89829219fc791573618f5816a8e92bb
César Jaldin <rodia.piedra@gmail.com>
2020-12-28 20:27:10 -0400

    Update the list of pending

--- src/main/java/com/workplace/simon/controller/BaseLineController.java
+++ src/main/java/com/workplace/simon/controller/BaseLineController.java
@@ -2,9 +2,10 @@ package com.workplace.simon.controller;
 
 import com.workplace.simon.model.BaseLine;
 import com.workplace.simon.model.BaseLineResource;
-import com.workplace.simon.model.Register;
+import com.workplace.simon.model.User;
 import com.workplace.simon.service.BaseLineService;
 import com.workplace.simon.service.RegisterService;
+import com.workplace.simon.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,12 +24,19 @@ public class BaseLineController {
     @Autowired
     private RegisterService registerService;
 
+    @Autowired
+    private UserService userService;
+
     public BaseLineService getBaseLineService() {
         return baseLineService;
     }
 
     public RegisterService getRegisterService() {
         return registerService;
+    }
+
+    public UserService getUserService() {
+        return userService;
     }
 
     private static final String AJAX_HEADER_NAME = "X-Requested-With";
@@ -39,7 +47,7 @@ public class BaseLineController {
         BaseLine baseLine = new BaseLine();
         baseLine.getResources().add(new BaseLineResource());
         baseLine.setSource(userId);
-        Register currentUser = this.getRegisterService().findById(baseLine.getSource())
+        User currentUser = this.getUserService().findById(baseLine.getSource())
                 .orElseThrow(() -> new IllegalArgumentException("Invalid user id : " + baseLine.getSource()));
 
         model.addAttribute("baseLine", baseLine);
@@ -58,7 +66,7 @@ public class BaseLineController {
             Model model
     ) {
         if (bindingResult.hasErrors()) {
-            Register currentUser = this.getRegisterService().findById(baseLine.getSource())
+            User currentUser = this.getUserService().findById(baseLine.getSource())
                     .orElseThrow(() -> new IllegalArgumentException("Invalid user id : " + baseLine.getSource()));
             model.addAttribute("currentUser", currentUser);
             model.addAttribute("allUsers", this.getRegisterService().findAll());
@@ -112,7 +120,7 @@ public class BaseLineController {
     ) {
         if (bindingResult.hasErrors()) {
             baseLine.setId(id);
-            Register currentUser = this.getRegisterService().findById(baseLine.getSource())
+            User currentUser = this.getUserService().findById(baseLine.getSource())
                     .orElseThrow(() -> new IllegalArgumentException("Invalid user id : " + baseLine.getSource()));
             model.addAttribute("currentUser", currentUser);
             model.addAttribute("allUsers", this.getRegisterService().findAll());
@@ -139,7 +147,7 @@ public class BaseLineController {
     ) {
         BaseLine baseLine = this.getBaseLineService().findById(baseLineId)
                 .orElseThrow(() -> new IllegalArgumentException("The id to gets the baseline record is not exists."));
-        Register currentUser = this.getRegisterService().findById(baseLine.getSource())
+        User currentUser = this.getUserService().findById(baseLine.getSource())
                 .orElseThrow(() -> new IllegalArgumentException("Invalid user id : " + baseLine.getSource()));
 
         model.addAttribute("baseLine", baseLine);
