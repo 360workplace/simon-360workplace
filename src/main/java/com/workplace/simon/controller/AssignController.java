355c54aa0892369083824860855b57d601937fcc
César Jaldin <rodia.piedra@gmail.com>
2020-12-30 19:10:12 -0400

    Add save process to save an assign request

--- src/main/java/com/workplace/simon/controller/AssignController.java
+++ src/main/java/com/workplace/simon/controller/AssignController.java
@@ -2,6 +2,7 @@ package com.workplace.simon.controller;
 
 import com.workplace.simon.model.Resource;
 import com.workplace.simon.model.Source;
+import com.workplace.simon.model.SourceType;
 import com.workplace.simon.model.User;
 import com.workplace.simon.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +10,12 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/assign/")
@@ -31,6 +36,7 @@ public class AssignController {
         source.getResources().add(new Resource());
         User user = this.getUserService().findByUsername(userDetails.getUsername());
         source.setUserId(user.getId());
+        source.setType(SourceType.ASSIGN_REQUEST);
 
         model.addAttribute("assign", source);
         model.addAttribute("currentUser", user);
@@ -38,5 +44,24 @@ public class AssignController {
         model.addAttribute("userid", user.getId());
 
         return "assign-request-form";
+    }
+
+    @PostMapping(params = "save", path = "add")
+    public String addBaseline(
+            @AuthenticationPrincipal UserDetails userDetails,
+            @Valid Source assign,
+            BindingResult bindingResult,
+            Model model
+    ) {
+        if (bindingResult.hasErrors()) {
+            User currentUser = this.getUserService().findByUsername(userDetails.getUsername());
+            model.addAttribute("currentUser", currentUser);
+            model.addAttribute("allUsers", this.getUserService().findAll());
+            model.addAttribute("userid", currentUser.getId());
+
+            return "assign-request-form";
+        }
+
+        return "redirect:/";
     }
 }
