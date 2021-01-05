9bdd9c595187310b41cf4953e166a711cffd6fa5
César Jaldin <rodia.piedra@gmail.com>
2021-01-05 11:49:49 -0400

    Update the area for each registered user

--- src/main/java/com/workplace/simon/repository/RoleRepository.java
+++ src/main/java/com/workplace/simon/repository/RoleRepository.java
@@ -2,6 +2,8 @@ package com.workplace.simon.repository;
 
 import com.workplace.simon.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 }
