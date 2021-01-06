5545006b4c21e334661c4c4b68a309ca12e05cd7
César Jaldin <rodia.piedra@gmail.com>
2021-01-06 13:28:55 -0400

    Fix sql query causing missing id fieldset

--- src/main/java/com/workplace/simon/repository/SourceRepository.java
+++ src/main/java/com/workplace/simon/repository/SourceRepository.java
@@ -13,6 +13,6 @@ import java.util.List;
 public interface SourceRepository extends JpaRepository<Source, Long> {
     List<Source> findByActiveTrue();
 
-    @Query(value = "SELECT source0_.id as id1_4_, source0_.active as active2_4_, source0_.detail as detail3_4_, source0_.fecha_inicial as fecha_in4_4_, source0_.titulo as titulo5_4_, source0_.type as type6_4_, source0_.fuente as fuente7_4_, source0_.responsable as responsa8_4_ from linea_base source0_ INNER JOIN users u ON (source0_.fuente = u.id) INNER JOIN area a ON (u.area_id = a.id) WHERE a.id = (:areaId) AND source0_.active=true", nativeQuery = true)
+    @Query(value = "SELECT source0_.id as id, source0_.active as active, source0_.detail as detail, source0_.fecha_inicial as fecha_inicial, source0_.titulo as titulo, source0_.type as type, source0_.fuente as fuente, source0_.responsable as responsable from linea_base source0_ INNER JOIN users u ON (source0_.fuente = u.id) INNER JOIN area a ON (u.area_id = a.id) WHERE a.id = (:areaId) AND source0_.active=true", nativeQuery = true)
     List<Source> findByArea(@Param("areaId") Long id);
 }
