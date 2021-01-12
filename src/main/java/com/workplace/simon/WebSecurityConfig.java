00c64921065c79dcb53e3c916a4bab39f50e784c
César Jaldin <rodia.piedra@gmail.com>
2021-01-12 09:14:34 -0400

    Update login keeping

--- src/main/java/com/workplace/simon/WebSecurityConfig.java
+++ src/main/java/com/workplace/simon/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -30,6 +31,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/assets/**").permitAll()
                 .anyRequest().authenticated()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
                 .and()
                 .formLogin()
                 .loginPage("/login")
--- src/main/java/com/workplace/simon/controller/ManagerReportController.java
+++ src/main/java/com/workplace/simon/controller/ManagerReportController.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -34,6 +35,7 @@ public class ManagerReportController {
         return keepSessionService;
     }
 
+    @GetMapping("week/report")
     public String printWeeklyReport(
             @AuthenticationPrincipal UserDetails userDetails,
             Model model
